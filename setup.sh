39dbf9f4b8b713ac1c78cee992e7e7dbdc9dafc9
Jaime Leon <jleonhol@gmail.com>
2022-03-11 12:55:14 +0100

    Modified setup

--- setup.sh
+++ setup.sh
@@ -283,7 +283,11 @@ action() {
         if [ "$CMT_FORCE_SOFTWARE" = "1" ]; then
             cmt_setup_software force
         else
-            cmt_setup_software silent
+	    if [ "$CMT_FORCE_CMSSW" = "1" ]; then
+                cmt_setup_software force_cmssw
+	    else
+	        cmt_setup_software silent
+	    fi
         fi
     fi
 
