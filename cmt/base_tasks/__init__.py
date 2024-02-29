02d3b38ed7738c7e474dec5067c4d37f1393a04b
Jaime Leon Holgado <jleonhol@gmail.com>
2024-02-29 16:45:50 +0000

    Moving cutflow tasks away from preprocessing.py

--- cmt/base_tasks/__init__.py
+++ cmt/base_tasks/__init__.py
@@ -1,4 +1,5 @@
 from cmt.base_tasks.preprocessing import *
+from cmt.base_tasks.cutflow import *
 from cmt.base_tasks.shards import *
 from cmt.base_tasks.plotting import *
 from cmt.base_tasks.analysis import *
--- cmt/base_tasks/preprocessing.py
+++ cmt/base_tasks/preprocessing.py
@@ -1031,6 +1031,7 @@ class MergeCategorization(DatasetTaskWithCategory, law.tasks.ForestMerge):
                 tf = ROOT.TFile.Open(create_file_dir(tmp_out.path), "RECREATE")
                 tf.Close()
 
+
 class MergeCategorizationWrapper(DatasetCategorySystWrapperTask):
     """
     Wrapper task to run the MergeCategorizationWrapper task over several datasets in parallel.
@@ -1046,70 +1047,6 @@ class MergeCategorizationWrapper(DatasetCategorySystWrapperTask):
             category_name=category.name, systematic=systematic,
             systematic_direction=direction)
 
-class MergeCutFlow(DatasetTaskWithCategory, law.tasks.ForestMerge):
-    """
-    Merges the output from the PreProcessRDF task if the option
-    compute-filter-efficiency is used.
-    """ 
-
-    # regions not supported
-    region_name = None
-
-    merge_factor = -1
-
-    def merge_workflow_requires(self):
-        return PreprocessRDF.vreq(self, compute_filter_efficiency=True, 
-                _prefer_cli=["workflow"])
-
-    def merge_requires(self, start_leaf, end_leaf):
-        return PreprocessRDF.vreq(self, compute_filter_efficiency=True, 
-                workflow="local", branches=((start_leaf, end_leaf),), _exclude={"branch"})
-
-    def trace_merge_inputs(self, inputs):
-        return [inp for inp in inputs["collection"].targets.values()]
-
-    def merge_output(self):
-        return self.local_target(f"cut_flow.json")
-
-    def merge(self, inputs, output):
-        # output content
-        stats = defaultdict(lambda: {"pass": 0, "all": 0})
-
-        # merge
-        for inp in inputs:
-            try:
-                if "json" in inp["cut_flow"].path:
-                    _stats = inp["cut_flow"].load(formatter="json")
-                else:
-                    continue
-            except:
-                print("error leading input target {}".format(inp["cut_flow"]))
-                raise
-
-            # add pass and all
-            if "json" in inp["cut_flow"].path:
-                for cutName, cutEffs in _stats.items():
-                    for keyPassAll, val in cutEffs.items():
-                        stats[cutName][keyPassAll] += val
-            else:
-                continue
-
-        output.parent.touch()
-        output.dump(stats, indent=4, formatter="json")
-
-class MergeCutFlowWrapper(DatasetCategorySystWrapperTask):
-    """
-    Wrapper task to run the MergeCutFlow task over several datasets in parallel.
-
-    Example command:
-
-    ``law run MergeCutFlowWrapper --version test --category-names etau \
---config-name base_config --dataset-names tt_dl,tt_sl --workers 10``
-    """
-
-    def atomic_requires(self, dataset, category, systematic, direction):
-        return MergeCutFlow.vreq(self, dataset_name=dataset.name,
-            category_name=category.name)
 
 class MergeCategorizationStats(DatasetTask, law.tasks.ForestMerge):
     """
