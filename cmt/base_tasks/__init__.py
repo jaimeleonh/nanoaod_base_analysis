d693d5c9bada64b65fb9771b676bc54c37f18426
diegof <diego.fernandez.del.val@cern.ch>
2024-05-16 16:43:04 +0200

    Cherry-pick of commit 7e8113e on top of the current global HEAD

--- cmt/base_tasks/__init__.py
+++ cmt/base_tasks/__init__.py
@@ -5,3 +5,4 @@ from cmt.base_tasks.plotting import *
 from cmt.base_tasks.analysis import *
 from cmt.base_tasks.training import *
 from cmt.base_tasks.optimization import *
+from cmt.base_tasks.skimming import *
