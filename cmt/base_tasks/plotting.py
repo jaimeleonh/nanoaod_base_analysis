164888bed38edf288ef49106bd7e4d1e95b88468
Jaime Leon <jleonhol@gmail.com>
2026-01-28 11:58:44 +0100

    Include process label in 2D inner text

--- cmt/base_tasks/plotting.py
+++ cmt/base_tasks/plotting.py
@@ -3031,11 +3031,6 @@ class FeaturePlot2D(FeaturePlot, BasePlotMultiDTask):
                 self.config.ecm,
             )
 
-        draw_labels = get_labels(
-            upper_right=upper_right,
-            inner_text=inner_text
-        )
-
         for ih, hist in enumerate(draw_hists):
             c = Canvas()
             if self.log_z:
@@ -3043,6 +3038,11 @@ class FeaturePlot2D(FeaturePlot, BasePlotMultiDTask):
             dummy_hist.SetMaximum(hist.GetMaximum())
             dummy_hist.Draw()
             hist.Draw("COLZ,SAME")
+
+            draw_labels = get_labels(
+                upper_right=upper_right,
+                inner_text=inner_text + [self.config.processes.get(hist.cmt_process_name).label]
+            )
             for label in draw_labels:
                 label.Draw("same")
 
